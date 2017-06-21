f59fff172480e2f8d5789a6211324bf752cce9c5
0937452 <0937452@hr.nl>
2017-06-21 17:02:09 +0200

    .

---
+++ RecipeApp/RecipeApp/RecipeApp/MainRecipePage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RecipeApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainRecipePage : ContentPage
+    {
+        public MainRecipePage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+    }
+}
