184c8fd70071894c101d61c6eb84b69111799031
Osman Altun <osman@145-24-193-28.wlan.hro.nl>
2017-06-22 14:00:47 +0200

    added visitor

--- RecipeApp/RecipeApp/RecipeApp/Utils.cs
+++ RecipeApp/RecipeApp/RecipeApp/Utils.cs
@@ -7,17 +7,36 @@ namespace RecipeApp
     {
         public static ContentPage Create(int option)
         {
-            switch(option)
+            switch (option)
             {
                 case 1:
                     return new MainPage();
-				case 2:
+                case 2:
                     throw new NotImplementedException();
-				case 3:
+                case 3:
                     throw new NotImplementedException();
                 default:
                     return new MainPage();
-			}
+            }
         }
+
+        public interface Visitable
+        {
+            void Visit(Visitor visitor);
+        }
+
+        public interface Visitor
+        {
+            void Accept(Recipe recipe);
+        }
+
+        public class Recipe : Visitable
+        {
+            public void Visit(Visitor visitor)
+            {
+                visitor.Accept(this);
+            }
+        }
+
     }
 }
