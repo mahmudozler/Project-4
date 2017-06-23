6f1e0b9dfcfffaff28522b38937c9b3c5197e608
Osman Altun <osman@145-24-193-28.wlan.hro.nl>
2017-06-23 14:21:59 +0200

    .

--- RecipeApp/RecipeApp/RecipeApp/Utils.cs
+++ RecipeApp/RecipeApp/RecipeApp/Utils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using Xamarin.Forms;
 
 namespace RecipeApp
@@ -10,7 +10,7 @@ namespace RecipeApp
             switch (option)
             {
                 case 1:
-                    return new BrowsePage();
+                    return new MainPage();
                 case 2:
                     throw new NotImplementedException();
                 case 3:
