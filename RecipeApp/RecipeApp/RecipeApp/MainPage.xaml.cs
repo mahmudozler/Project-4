19cb122176dfee7047e0436c82704da7cfd421d2
Osman Altun <osman@145-24-193-28.wlan.hro.nl>
2017-06-23 12:52:07 +0200

    namespace fix

--- RecipeApp/RecipeApp/RecipeApp/MainPage.xaml.cs
+++ RecipeApp/RecipeApp/RecipeApp/MainPage.xaml.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace RecipeApp
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
         public MainPage()
         {
--- RecipeApp/RecipeApp/RecipeApp/Utils.cs
+++ RecipeApp/RecipeApp/RecipeApp/Utils.cs
@@ -19,107 +19,106 @@ namespace RecipeApp
                     return new MainPage();
             }
         }
+    }
 
-		public interface Option<T>
+	public interface Option<T>
+	{
+		U Visit<U>(Func<T, U> OnSome, Func<U> OnNone);
+		void Visit(Action<T> OnSome, Action OnNone);
+	}
+
+
+
+	public interface Iterator<T>
+	{
+		Option<T> GetNext();
+		void Reset();
+		Option<T> GetCurrent();
+	}
+
+	public class Some<T> : Option<T>
+	{
+		private T Value;
+
+		public Some(T item)
 		{
-			U Visit<U>(Func<T, U> OnSome, Func<U> OnNone);
-			void Visit(Action<T> OnSome, Action OnNone);
+			this.Value = item;
 		}
 
-
-
-		public interface Iterator<T>
+		public U Visit<U>(Func<T, U> OnSome, Func<U> OnNone)
 		{
-			Option<T> GetNext();
-			void Reset();
-			Option<T> GetCurrent();
+			return OnSome(this.Value);
 		}
 
-		public class Some<T> : Option<T>
+		public void Visit(Action<T> OnSome, Action OnNone)
 		{
-			private T Value;
+			OnSome(this.Value);
+		}
+	}
 
-			public Some(T item)
-			{
-				this.Value = item;
-			}
-
-			public U Visit<U>(Func<T, U> OnSome, Func<U> OnNone)
-			{
-				return OnSome(this.Value);
-			}
-
-			public void Visit(Action<T> OnSome, Action OnNone)
-			{
-				OnSome(this.Value);
-			}
+	public class None<T> : Option<T>
+	{
+		public U Visit<U>(Func<T, U> OnSome, Func<U> OnNone)
+		{
+			return OnNone();
 		}
 
-		public class None<T> : Option<T>
+		public void Visit(Action<T> OnSome, Action OnNone)
 		{
-			public U Visit<U>(Func<T, U> OnSome, Func<U> OnNone)
-			{
-				return OnNone();
-			}
+			OnNone();
+		}
+	}
 
-			public void Visit(Action<T> OnSome, Action OnNone)
-			{
-				OnNone();
-			}
+	public class List<T> : Iterator<T>
+	{
+		public System.Collections.Generic.List<T> Elements = new System.Collections.Generic.List<T>();
+		public int Current = -1;
+
+		public void Add(T value)
+		{
+			Elements.Add(value);
 		}
 
-		public class List<T> : Iterator<T>
+		public Option<T> GetCurrent()
 		{
-			public System.Collections.Generic.List<T> Elements = new System.Collections.Generic.List<T>();
-			public int Current = -1;
-
-			public void Add(T value)
+			try
 			{
-				Elements.Add(value);
+				return new Some<T>(Elements[Current]);
 			}
-
-			public Option<T> GetCurrent()
-			{
-				try
-				{
-					return new Some<T>(Elements[Current]);
-				}
-				catch (ArgumentOutOfRangeException) { return new None<T>(); }
-			}
-
-			public Option<T> GetNext()
-			{
-				this.Current += 1;
-				return GetCurrent();
-			}
-
-			public void Reset()
-			{
-				this.Current = -1;
-			}
+			catch (ArgumentOutOfRangeException) { return new None<T>(); }
 		}
 
-        public class Recipe
+		public Option<T> GetNext()
+		{
+			this.Current += 1;
+			return GetCurrent();
+		}
+
+		public void Reset()
+		{
+			this.Current = -1;
+		}
+	}
+
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string ImageURL { get; set; }
+
+        public Recipe(int index)
         {
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public List<string> Ingredients { get; set; }
-            public string ImageURL { get; set; }
-
-            public Recipe(int index)
-            {
-                
-            }
-
-            public Option<Recipe> Filter(string str)
-            {
-                if (Ingredients.Elements.Contains(str))
-                {
-                    return new Some<Recipe>(this);
-                }
-                else return new None<Recipe>();
-            }
+            
         }
 
+        public Option<Recipe> Filter(string str)
+        {
+            if (Ingredients.Elements.Contains(str))
+            {
+                return new Some<Recipe>(this);
+            }
+            else return new None<Recipe>();
+        }
     }
 }
