456ea13c7afdf029bb292c46f8c0937deefb38a6
Satrya Sabeni <s.sabeni17@gmail.com>
2017-06-30 01:40:09 +0200

    Basic login feature
    logic mf

---
+++ Basic Login /AccountPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SQLform
+{
+    public partial class AccountPage : ContentPage
+    {
+        public AccountPage()
+        {
+			InitializeComponent();
+			if ((string)Application.Current.Properties["status"] == "logged_in")
+			{
+				var username = Application.Current.Properties["username"].ToString();
+				var password = Application.Current.Properties["password"].ToString();
+				var admin = Application.Current.Properties["admin"].ToString();
+
+				InitializeComponent();
+				user.Text = "user: " + username;
+				pass.Text = "password: " + password;
+				ad.Text = "admin: " + admin;
+			}
+			else
+			{
+				status.Text = "Log in first to check account details";
+			}
+        }
+    }
+}
---
+++ Basic Login /LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+using Xamarin.Forms;
+
+namespace SQLform
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+			Application.Current.Properties["status"] = "logged_out"; // status to see if a user is logged in or not
+			InitializeComponent();
+
+            tolog.Command = new Command(() => Navigation.PushAsync(new AccountPage()));
+			login.Command = new Command(getUser);
+        }
+
+        // User login check
+        private async void getUser()
+        {
+            var client = new HttpClient();
+            var data = await client.GetStringAsync("http://infpr04.esy.es/login.php?user=" + log_name.Text + "&pass=" + log_pass.Text);
+            var user = JsonConvert.DeserializeObject<List<User>>(data);
+
+            //Check if valid login
+            if (user.Count == 0)
+            {
+                log_status.TextColor = Color.Red;
+                log_status.Text = "Invalid login";
+            }
+            else
+            {
+                // If login matches user in db change status to logged in 
+                Application.Current.Properties["status"] = "logged_in";
+
+                // User variables accesible throughout whole application 
+                Application.Current.Properties["username"] = user[0].username;
+                Application.Current.Properties["password"] = user[0].password;
+                Application.Current.Properties["admin"] = user[0].admin;
+
+                log_status.TextColor = Color.Green;
+                log_status.Text = "Succesfull login!";
+            }
+        }
+
+		public class User
+		{
+			public string username { get; set; }
+			public string password { get; set; }
+			public string admin { get; set; }
+		}
+    }
+}
