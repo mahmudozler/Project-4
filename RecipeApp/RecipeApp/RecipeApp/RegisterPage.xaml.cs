b481a2914a7ad12afaa5c02aaa0a05770667620c
Osman Altun <osman@145-24-193-28.wlan.hro.nl>
2017-06-30 15:21:06 +0200

    Revert "Robuustheidscheck"
    This reverts commit 4ddfceef799e5eb381cc926fd9165fbacf3216bc.

--- RecipeApp/RecipeApp/RecipeApp/RegisterPage.xaml.cs
+++ RecipeApp/RecipeApp/RecipeApp/RegisterPage.xaml.cs
@@ -11,27 +11,5 @@ namespace RecipeApp
         {
             InitializeComponent();
         }
-        void Entry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (e.NewTextValue.Length > 20)
-            {
-                EntryInput.Text = EntryInput.Text.Remove(20);
-            }
-        }
-        void Password_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (e.NewTextValue.Length > 20)
-            {
-                PasswordInput1.Text = PasswordInput1.Text.Remove(20);
-            }
-        }
-        void Password2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (e.NewTextValue.Length > 20)
-            {
-                PasswordInput2.Text = PasswordInput2.Text.Remove(20);
-            }
-        }
     }
-    
 }
