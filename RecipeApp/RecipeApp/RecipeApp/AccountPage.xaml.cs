eff7562b9b12ed8e505de8d1ff2bbf510e4e61d3
Satrya Sabeni <s.sabeni17@gmail.com>
2017-07-01 11:43:55 +0200

    Implementing login 1

--- RecipeApp/RecipeApp/RecipeApp/AccountPage.xaml.cs
+++ RecipeApp/RecipeApp/RecipeApp/AccountPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,5 +18,37 @@ namespace RecipeApp
 		{
 			InitializeComponent ();
         }
+
+        //Validate login attempt
+		private async void getUser()
+		{
+			var client = new HttpClient();
+			var data = await client.GetStringAsync("http://infpr04.esy.es/login.php?user=" + log_name.Text + "&pass=" + log_pass.Text);
+			var user = JsonConvert.DeserializeObject<List<User>>(data);
+
+			//Check if valid login
+			if (user.Count == 0)
+			{
+				login_response.TextColor = Color.Red;
+				login_response.Text = "Invalid login";
+			}
+			else
+			{
+				// If login matches user in db change status to logged in 
+				Global.status = "logged_in";
+
+				// User variables accesible throughout whole application 
+				Global.username = user[0].username;
+				Global.password = user[0].password;
+				Global.admin = Int32.Parse(user[0].admin);
+
+				login_response.TextColor = Color.Green;
+				login_response.Text = "Succesfull login!";
+
+				//login_page.Children.Clear();
+
+				//login_page.Children.Add(new Button { Text = "go to account", Command = new Command(() => Application.Current.MainPage = new AccountPage()) });
+			}
+		}
     }
 }
---
+++ RecipeApp/RecipeApp/RecipeApp/GlobalData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RecipeApp
+{
+	public static class Global
+	{
+		public static string status = "logged_out";
+		public static string username { get; set; }
+		public static string password { get; set; }
+		public static int admin { get; set; }
+	}
+}
